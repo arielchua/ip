d1d7dbca7b917a32bd1508cca15e4a9e358bd552
ariel <arielchua138@gmail.com>
2026-01-22 17:50:54 +0800

    Changed name and added greetings

---
+++ src/main/java/ChuaChua.java
@@ -0,0 +1,10 @@
+public class ChuaChua {
+    public static void main(String[] args) {
+        String logo = "_____________________________________ \n"
+                + "Hello! I'm ChuaChua \n"
+                + "What can I do for you? \n"
+                + "_____________________________________ \n"
+                + "Bye. Hope to see you again soon!";
+        System.out.println(logo);
+    }
+}
