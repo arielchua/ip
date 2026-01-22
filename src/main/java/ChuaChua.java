ff3b8730dc190f1f2e4582528c14764b9be19770
ariel <arielchua138@gmail.com>
2026-01-23 04:29:57 +0800

    Updated Level 3 for extension

--- src/main/java/ChuaChua.java
+++ src/main/java/ChuaChua.java
@@ -10,46 +10,40 @@ public class ChuaChua {
         System.out.println(intro);
 
         //add list
-        String[] listItem = new String[100];
-        boolean[] listBool = new boolean[100];
+        Task[] tasks = new Task[100];
         int count = 0;
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                String output = printList(listItem, listBool, count);
+                String output = printList(tasks, count);
                 System.out.println(indent + output);
             } else if (input.startsWith("mark")) {
                 int toMark = Integer.parseInt(input.substring(5));
-                listBool[toMark] = true;                    //true = marked
-                System.out.println(indent + "Great! I've marked this task as done:\n [X] " + listItem[toMark]);
+                tasks[toMark].mark();
+                System.out.println(indent + "Great! I've marked this task as done:\n" + tasks[toMark]);
             } else if (input.startsWith("unmark")) {
                 int toUnMark = Integer.parseInt(input.substring(7));
-                listBool[toUnMark] = false;                    //true = marked
-                System.out.println(indent + "OK, I've marked this task as not done yet::\n [ ] " + listItem[toUnMark]);
+                tasks[toUnMark].unmark();
+                System.out.println(indent + "OK, I've marked this task as not done yet:\n" + tasks[toUnMark]);
             } else {
                 count++;
-                listItem[count] = input;
-                listBool[count] = false;
+                tasks[count] = new Task(input);
                 System.out.println(indent + "added: " + input);
             }
             input = sc.nextLine();
         }
+
         String outro = indent + "Oh bye bye! See you soon!";
         System.out.println(outro);
 
     }
 
-    public static String printList(String[] ls, boolean[] bool, int count) {
+    public static String printList(Task[] tasks, int count) {
         String output = "Here are the tasks in your list:\n";
-        String marked = "[X] ";
-        String unmarked = "[ ] ";
         for (int i = 1; i <= count; i++) {
-            if (bool[i]) {
-                output += i + ". " + marked + ls[i] + "\n";
-            } else {
-                output += i + ". " + unmarked + ls[i] + "\n";
-            }
+            output += i + ". " + tasks[i].toString() + "\n";
         }
         return output;
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    public String taskName;
+    public boolean status;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.status = false;
+    }
+
+    public void mark() {
+        this.status = true;
+    }
+
+    public void unmark() {
+        this.status = false;
+    }
+
+    public String toString() {
+        if (status) {
+            return "[X] " + taskName;
+        } else {
+            return "[ ] " + taskName;
+        }
+    }
+
+}
