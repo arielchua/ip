1a9b47124e186f2d36d43d1c883f0f18facae78f
ariel <arielchua138@gmail.com>
2026-01-22 20:15:00 +0800

    Added list function

--- src/main/java/ChuaChua.java
+++ src/main/java/ChuaChua.java
@@ -4,21 +4,37 @@ import java.util.Scanner;
 public class ChuaChua {
     public static void main(String[] args) {
         //introduction
-        String indent = "CC: ";
+        String indent = "CC:---------------------------------------\n";
         String intro = indent + "Hi! I'm ChuaChua \n"
-                + indent + "How can I help you?";
+                + "How can I help you?";
         System.out.println(intro);
 
-        //echoing
+        //add list
+        String[] list = new String[100];
+        int count = 0;
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        while (!Objects.equals(input, "bye")) {
-            System.out.println(indent + input);
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                String output = printList(list, count);
+                System.out.println(indent + output);
+            } else {
+                count++;
+                list[count] = input;
+                System.out.println(indent + "added: " + input);
+            }
             input = sc.nextLine();
         }
-
-        //exit message
         String outro = indent + "Oh bye bye! See you soon!";
         System.out.println(outro);
+
+    }
+
+    public static String printList(String[] ls, int count) {
+        String output = "";
+        for (int i = 1; i <= count; i++) {
+            output += i + ". " + ls[i] + "\n";
+        }
+        return output;
     }
 }
