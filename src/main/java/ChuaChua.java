7ce8a9c80d4c211a5532df639eb963869c4e57c4
ariel <arielchua138@gmail.com>
2026-01-22 18:26:12 +0800

    Updated echo

--- src/main/java/ChuaChua.java
+++ src/main/java/ChuaChua.java
@@ -4,23 +4,21 @@ import java.util.Scanner;
 public class ChuaChua {
     public static void main(String[] args) {
         //introduction
-        String divider = "\n___________________________________\n";
-        String intro = "Hi! I'm ChuaChua \n"
-                + "How can I help you?"
-                + divider;
-              //  + "Bye. Hope to see you again soon!";
+        String indent = "CC: ";
+        String intro = indent + "Hi! I'm ChuaChua \n"
+                + indent + "How can I help you?";
         System.out.println(intro);
 
         //echoing
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!Objects.equals(input, "bye")) {
-            System.out.println(input + divider);
+            System.out.println(indent + input);
             input = sc.nextLine();
         }
 
         //exit message
-        String outro = "Oh bye bye! See you soon!";
+        String outro = indent + "Oh bye bye! See you soon!";
         System.out.println(outro);
     }
 }
