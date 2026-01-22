b6cb9e6df473ff52bec80f335b3301d693f30e97
ariel <arielchua138@gmail.com>
2026-01-22 18:13:09 +0800

    Added echo function

--- src/main/java/ChuaChua.java
+++ src/main/java/ChuaChua.java
@@ -1,10 +1,26 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class ChuaChua {
     public static void main(String[] args) {
-        String logo = "_____________________________________ \n"
-                + "Hello! I'm ChuaChua \n"
-                + "What can I do for you? \n"
-                + "_____________________________________ \n"
-                + "Bye. Hope to see you again soon!";
-        System.out.println(logo);
+        //introduction
+        String divider = "\n___________________________________\n";
+        String intro = "Hi! I'm ChuaChua \n"
+                + "How can I help you?"
+                + divider;
+              //  + "Bye. Hope to see you again soon!";
+        System.out.println(intro);
+
+        //echoing
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!Objects.equals(input, "bye")) {
+            System.out.println(input + divider);
+            input = sc.nextLine();
+        }
+
+        //exit message
+        String outro = "Oh bye bye! See you soon!";
+        System.out.println(outro);
     }
 }
