355c64723cb8b03baf92b47bd85231c7afa53e91
ariel <arielchua138@gmail.com>
2026-01-24 17:43:59 +0800

    Task 5 completed

--- src/main/java/ChuaChua.java
+++ src/main/java/ChuaChua.java
@@ -1,76 +1,111 @@
-import java.util.Objects;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ChuaChua {
     public static void main(String[] args) {
         //introduction
         String indent = "CC:---------------------------------------\n";
-        String intro = indent + "Hi! I'm ChuaChua \n"
+        String intro = indent + "Hi! I'm ChuaChua\n"
                       + "How can I help you?";
         System.out.println(intro);
 
-        //add list
-        Task[] tasks = new Task[100];
-        int count = 0;
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+//        //add list
+//        Task[] tasks = new Task[100];
+//        int count = 0;
 
-        while (!input.equals("bye")) {
-            if (input.equals("list")) {
-                String output = printList(tasks, count);
-                System.out.println(indent + output);
-            } else if (input.startsWith("mark")) {
-                int toMark = Integer.parseInt(input.substring(5));
-                tasks[toMark].mark();
-                System.out.println(indent + "Great! I've marked this task as done:\n" + tasks[toMark]);
-            } else if (input.startsWith("unmark")) {
-                int toUnMark = Integer.parseInt(input.substring(7));
-                tasks[toUnMark].unmark();
-                System.out.println(indent + "OK, I've marked this task as not done yet:\n" + tasks[toUnMark]);
-            } else {
-                count++;
-                if (input.startsWith("todo")) {
-                    tasks[count] = new ToDos(input.substring(5));
-                    System.out.println(indent + adding(tasks[count], count));
+        Scanner sc = new Scanner(System.in);
+        //change to arraylist
+        ArrayList<Task> tasks = new ArrayList<Task>();
+
+        while (true) {
+            String input = sc.nextLine();
+
+            try {
+                validate(input);
+
+                if (input.equals("bye")) {
+                    break;
+                } else if (input.equals("list")) {
+                    String output = printList(tasks);
+                    System.out.println(indent + output);
+
+                } else if (input.startsWith("mark")) {
+                    int toMark = Integer.parseInt(input.substring(5));
+                    tasks.get(toMark).mark();
+                    System.out.println(indent + "Great! I've marked this task as done:\n" + tasks.get(toMark));
+
+                } else if (input.startsWith("unmark")) {
+                    int toUnMark = Integer.parseInt(input.substring(7));
+                    tasks.get(toUnMark).unmark();
+                    System.out.println(indent + "OK, I've marked this task as not done yet:\n" + tasks.get(toUnMark));
+
+                } else if (input.startsWith("todo")) {
+                    tasks.add(new ToDos(input.substring(5)));
+                    System.out.println(indent + adding(tasks.get(tasks.size() - 1), tasks));
+
                 } else if (input.startsWith("deadline")) {
                     String rest = input.substring(9);
                     String[] parts = rest.split(" /by ", 2);
-                    tasks[count] = new DeadLines(parts[0], parts[1]);
-                    System.out.println(indent + adding(tasks[count], count));
+                    tasks.add(new DeadLines(parts[0], parts[1]));
+                    System.out.println(indent + adding(tasks.get(tasks.size() - 1), tasks));
+
                 } else if (input.startsWith("event")) {
                     String rest = input.substring(6);
                     String[] firstSplit = rest.split(" /from ", 2);
                     String[] secondSplit = firstSplit[1].split(" /to ", 2);
-                    tasks[count] = new Events(firstSplit[0], secondSplit[0], secondSplit[1]);
-                    System.out.println(indent + adding(tasks[count], count));
-                } else {
-                    tasks[count] = new Task(input);
-                    System.out.println(indent + adding(tasks[count], count));
+                    tasks.add(new Events(firstSplit[0], secondSplit[0], secondSplit[1]));
+                    System.out.println(indent + adding(tasks.get(tasks.size() - 1), tasks));
                 }
+
+            } catch (EmptyDescriptionException e) {
+                System.out.println(indent + e.getMessage());
+            } catch (UnknownCommandException e) {
+                System.out.println(indent + e.getMessage());
             }
-            input = sc.nextLine();
         }
+
 
         String outro = indent + "Oh bye bye! See you soon!";
         System.out.println(outro);
 
     }
 
-    public static String printList(Task[] tasks, int count) {
+    public static String printList(ArrayList<Task> tasks) {
         String output = "Here are the tasks in your list:\n";
-        for (int i = 1; i <= count; i++) {
-            output += i + ". " + tasks[i].toString() + "\n";
+        for (int i = 1; i <= tasks.size(); i++) {
+            output += i + ". " + tasks.get(i - 1).toString() + "\n";
         }
-        output += "Now you have " + count + " tasks in the list";
+        output += "Now you have " + tasks.size() + " tasks in the list";
         return output;
     }
 
-    public static String adding(Task task, int count) {
+    public static String adding(Task task, ArrayList<Task> tasks) {
         String output = "Okay, I've added the task:\n  "
                 + task.toString()
                 + "\nNow you have "
-                + count
+                + tasks.size()
                 + " tasks in the list";
         return output;
     }
+    public static void validate(String input)
+            throws EmptyDescriptionException, UnknownCommandException {
+
+        if (input.equals("todo") || input.equals("deadline") || input.equals("event")) {
+            throw new EmptyDescriptionException(
+                    "Please include the task description for me!"
+            );
+        }
+
+        if (!(input.startsWith("todo")
+                || input.startsWith("deadline")
+                || input.startsWith("event")
+                || input.equals("list")
+                || input.equals("bye"))) {
+
+            throw new UnknownCommandException(
+                    "Sorry I don't know what you mean :("
+            );
+        }
+    }
+
 }
